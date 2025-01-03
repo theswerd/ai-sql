d87817a16194830ddf382ed6a891d4065fa052ac
Will Hopkins <willothyh@gmail.com>
2025-01-02 22:20:16 -0800

    fix: flatten list of results

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import { tool } from "ai";
 import { z } from "zod";
-import { Client } from "pg";
+import { Client, QueryResult } from "pg";
 
 export const postgreSQLTool = async (database_url: string) => {
   const client = new Client(database_url);
   await client.connect();
 
   const result = await client.query(`
-    SELECT table_schema, 
-       table_name, 
+    SELECT table_schema,
+       table_name,
        string_agg(
-           format('%s %s %s', column_name, 
-                  CASE 
+           format('%s %s %s', column_name,
+                  CASE
                     WHEN data_type = 'character varying' THEN 'VARCHAR(' || character_maximum_length || ')'
                     WHEN data_type = 'numeric' THEN 'NUMERIC(' || numeric_precision || ',' || numeric_scale || ')'
                     WHEN data_type = 'character' THEN 'CHAR(' || character_maximum_length || ')'
-                    ELSE data_type 
-                  END, 
-                  CASE WHEN is_nullable = 'YES' THEN 'NULL' ELSE 'NOT NULL' END), 
+                    ELSE data_type
+                  END,
+                  CASE WHEN is_nullable = 'YES' THEN 'NULL' ELSE 'NOT NULL' END),
            ',\n  ' ORDER BY ordinal_position
        ) AS columns
 FROM information_schema.columns
@@ -43,7 +43,12 @@ CREATE TABLE ${table_schema}.${table_name} (
     description: `Query PostgreSQL Database with the following schema:\n\n${schema}`,
     execute: async (query) => {
       const result = await client.query(query.query!);
-      return result.rows;
+      console.log(result);
+
+      const res = Array.isArray(result) ? result : [result];
+      const rows = res.map((r: QueryResult) => r.rows);
+
+      return rows;
     },
     parameters: z.object({
       query: z.string().describe("SQL Query to execute"),
