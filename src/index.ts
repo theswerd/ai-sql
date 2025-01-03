c3ef92588bb7a16920e6530c08918667cfb68f82
Will Hopkins <willothyh@gmail.com>
2025-01-03 00:23:27 -0800

    feat: add optional notes prop to tools

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { PostgresTool } from "./providers/postgres";
-import { sqlTool } from "./database";
+import { sqlTool, SqlToolOptions } from "./database";
 
-export async function postgres(dbUrl: string) {
-  return await sqlTool(new PostgresTool(dbUrl));
+export async function postgres(dbUrl: string, options?: SqlToolOptions) {
+  return await sqlTool(new PostgresTool(dbUrl), options);
 }
